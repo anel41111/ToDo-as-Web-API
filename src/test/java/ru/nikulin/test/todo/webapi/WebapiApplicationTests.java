d6db93ee8272596d85273f75c434090b5a0ad8e4
Preposterone <ivan38rus@gmail.com>
2022-02-02 19:51:03 +0300

    Initial commit

---
+++ src/test/java/ru/nikulin/test/todo/webapi/WebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nikulin.test.todo.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
