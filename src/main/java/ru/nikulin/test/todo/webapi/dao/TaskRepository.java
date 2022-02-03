b22fd01f6ebe5cfe05214801871a106d94e8e401
Preposterone <ivan38rus@gmail.com>
2022-02-03 20:31:18 +0300

    Dao layer

---
+++ src/main/java/ru/nikulin/test/todo/webapi/dao/TaskRepository.java
@@ -0,0 +1,10 @@
+package ru.nikulin.test.todo.webapi.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.nikulin.test.todo.webapi.model.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+}
