ec5fbd6eb5e204e424c235114164fab7638ab0ef
Preposterone <ivan38rus@gmail.com>
2022-02-03 19:54:06 +0300

    Data model basic implementation

---
+++ src/main/java/ru/nikulin/test/todo/webapi/model/Project.java
@@ -0,0 +1,45 @@
+package ru.nikulin.test.todo.webapi.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "projects")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String projectName;
+
+    @Column
+    private LocalDateTime projectStartDate;
+
+    @Column
+    private LocalDateTime projectCompletionDate;
+
+    @Column
+    private ProjectStatus projectStatus;
+
+    @Column
+    private Integer priority;
+
+    @OneToMany
+    @OrderBy("id")
+    private List<Task> tasks;
+
+    public enum ProjectStatus {
+        NotStarted, Active, Completed
+    }
+}
---
+++ src/main/java/ru/nikulin/test/todo/webapi/model/Task.java
@@ -0,0 +1,36 @@
+package ru.nikulin.test.todo.webapi.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String taskName;
+
+    @Column(columnDefinition = "text")
+    private String description;
+
+    @Column
+    private Integer priority;
+
+    @Column
+    private TaskStatus taskStatus;
+
+    public enum TaskStatus {
+        ToDo, InProgress, Done
+    }
+}
