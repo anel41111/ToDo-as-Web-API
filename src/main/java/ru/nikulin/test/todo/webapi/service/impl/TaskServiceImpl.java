d9122d2c4d07f78bc7b9a9b6d05dcb11f25aab76
Preposterone <ivan38rus@gmail.com>
2022-02-05 20:28:08 +0300

    Closes #1

--- src/main/java/ru/nikulin/test/todo/webapi/service/impl/TaskServiceImpl.java
+++ src/main/java/ru/nikulin/test/todo/webapi/service/impl/TaskServiceImpl.java
@@ -1,24 +1,32 @@
 package ru.nikulin.test.todo.webapi.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-import ru.nikulin.test.todo.webapi.dao.ProjectRepository;
 import ru.nikulin.test.todo.webapi.dao.TaskRepository;
 import ru.nikulin.test.todo.webapi.dto.TaskDto;
 import ru.nikulin.test.todo.webapi.service.TaskService;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class TaskServiceImpl implements TaskService {
 
+    private final ModelMapper mapper;
     private final TaskRepository taskRepository;
-    private final ProjectRepository projectRepository;
 
     @Override
-    public List<TaskDto> getTasksByProjectId(Long projectId, Integer pageNo, Integer pageSize, String sortBy) {
-        return null;
+    public List<TaskDto> getTasksByProjectId(Long projectId) {
+        var result = taskRepository.findAllByProjectId(projectId);
+
+        if (result.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return result.stream().map(s -> mapper.map(s, TaskDto.class)).collect(Collectors.toList());
     }
 
     @Override
@@ -37,7 +45,10 @@ public class TaskServiceImpl implements TaskService {
     }
 
     @Override
-    public TaskDto getTask(Long taskId) {
-        return null;
+    public Optional<TaskDto> findTaskById(Long id) {
+        if (id == null || id <= 0) {
+            throw new IllegalArgumentException("Id cannot be null or <= 0");
+        }
+        return taskRepository.findById(id).map(s -> mapper.map(s, TaskDto.class));
     }
 }
