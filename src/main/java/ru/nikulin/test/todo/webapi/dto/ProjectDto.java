bfccabb066d1240d51c30017c12cb1d41271bbf6
Preposterone <ivan38rus@gmail.com>
2022-02-06 13:12:59 +0300

    #3 #4 added validation

--- src/main/java/ru/nikulin/test/todo/webapi/dto/ProjectDto.java
+++ src/main/java/ru/nikulin/test/todo/webapi/dto/ProjectDto.java
@@ -1,7 +1,13 @@
 package ru.nikulin.test.todo.webapi.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.lang.Nullable;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -11,11 +17,28 @@ import java.util.List;
 @Getter
 @EqualsAndHashCode(of = "id")
 public class ProjectDto {
+
+    @Null(message = "Do not specify id of a new project, it will be generated automatically")
     private Long id;
+
+    @NotBlank(message = "Name of a project must not be blank or empty")
     private String projectName;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="Europe/Moscow")
     private LocalDateTime projectStartDate;
+
+    @Nullable
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="Europe/Moscow")
     private LocalDateTime projectCompletionDate;
+
+    @NotNull(message = "Project status must be specified")
     private ProjectStatusDto projectStatus;
+
+    @NotNull(message = "Project priority must be specified")
     private Integer priority;
+
+    @NotNull
     private List<TaskDto> tasks;
 }
--- src/main/java/ru/nikulin/test/todo/webapi/dto/TaskDto.java
+++ src/main/java/ru/nikulin/test/todo/webapi/dto/TaskDto.java
@@ -1,6 +1,11 @@
 package ru.nikulin.test.todo.webapi.dto;
 
 import lombok.*;
+import org.springframework.lang.Nullable;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -8,9 +13,18 @@ import lombok.*;
 @Getter
 @EqualsAndHashCode(of = "id")
 public class TaskDto {
+    @Null(message = "Do not specify id of a new task, it will be generated automatically")
     private Long id;
+
+    @NotBlank(message = "Task name must not be blank or empty")
     private String taskName;
+
+    @Nullable
     private String description;
+
+    @NotNull(message = "Task priority must be specified")
     private Integer priority;
+
+    @NotNull(message = "Task status must be specified")
     private TaskStatusDto taskStatus;
 }
